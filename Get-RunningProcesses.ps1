ecad08b4ea5eaf4590e9405325080c9e7f8f54e6
Landon Diedrich <43480252+landonis@users.noreply.github.com>
2024-07-08 12:25:13 -0400

    Create Get-RunningProcesses.ps1

---
+++ Get-RunningProcesses.ps1
@@ -0,0 +1,15 @@
+$origin = tasklist /V /FO csv | ConvertFrom-CSV
+
+$output = New-Object PSCustomObject[] ($origin.Length)
+
+for ($i=0; $i -lt $origin.Length; $i++)
+{
+    $output[$i] = [PSCustomObject]@{
+        taskName = [String] $origin[$i].'Image Name'
+        user = [String] $origin[$i].'User Name'
+        pid = [Int] $origin[$i].PID
+        sessionType = [String] $origin[$i].'Session Name'
+        session = [Int] $origin[$i].'Session#'
+        memoryUsage = [Int] ($origin[$i].'Mem Usage' -replace '[a-z]')
+    }
+}
